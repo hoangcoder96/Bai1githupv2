aa9c9bd13cdd4f3738ee14e6db3bafd23492414d
hoangcoder96 <nhattocoder@gmail.com>
2017-03-19 23:35:22 +0700

    commit changed

---
+++ app/src/main/java/nhattocoder/com/bai1githupv2/LoginActivity.java
@@ -0,0 +1,9 @@
+package nhattocoder.com.bai1githupv2;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by User name on 19/03/2017.
+ */
+public class LoginActivity extends AppCompatActivity {
+}
---
+++ app/src/main/java/nhattocoder/com/bai1githupv2/Student.java
@@ -0,0 +1,7 @@
+package nhattocoder.com.bai1githupv2;
+
+/**
+ * Created by User name on 19/03/2017.
+ */
+public class Student {
+}
